b5b8ee87e4a4464de0d60381f862971d6db646b2
Matthew Steyn <steynie8@gmail.com>
2025-06-01 00:56:29 +0200

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+// Polyfill for AWS Amplify/crypto dependencies in browser
+(window as any).global = window;
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
